54c49df3727a49d456251526959d25329e848131
Lu <luabrxves@gmail.com>
2026-09-06 17:50:59 -0300

    feat: restaura pareamento da portaria ao abrir app

--- app/src/main/java/com/rondasafe/app/MainActivity.kt
+++ app/src/main/java/com/rondasafe/app/MainActivity.kt
@@ -3,12 +3,14 @@ package com.rondasafe.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.rondasafe.app.data.repository.PortariaRepository
 import com.rondasafe.app.ui.RondaSafeApp
 import com.rondasafe.app.ui.theme.RondaSafeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PortariaRepository.restoreDeviceCredential(this)
         setContent {
             RondaSafeTheme {
                 RondaSafeApp()
