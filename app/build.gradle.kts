8e94cd9e38f11c4af06b97ef458c00e62dcd98c3
Lu <luabrxves@gmail.com>
2026-09-09 10:36:58 -0300

    test: add sync policy test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,6 @@ dependencies {
 
     implementation("io.coil-kt.coil3:coil-compose:3.6.2")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.6.2")
+
+    testImplementation("junit:junit:4.13.2")
 }
